3aceca66af2f2dea3bf1859fcfb12c89f267ab85
rl0030 <r.lewis0030@gmail.com>
2025-03-27 15:48:15 -0400

    Copy config/env.development.js from Lupa

---
+++ config/env.development.js
@@ -0,0 +1,7 @@
+// env.development.js
+export default {
+  GOOGLE_MAPS_API_KEY: 'fake_key',
+  STRIPE_MERCHANT_IDENTIFIER: 'merchant.com.Lupa',
+  STRIPE_URL_SCHEME: 'lupa',
+  STRIPE_PUBLISHABLE_KEY: 'pk_test_99QhW2YbrvHJHLEItYbZ5rH7009Ha858ta',
+};
