92885b19b2e67bbb8310d367f7b91385fd0f70ff
rl0030 <r.lewis0030@gmail.com>
2025-03-27 15:54:30 -0400

    Copy types/env.d.ts from Lupa

---
+++ types/env.d.ts
@@ -0,0 +1,35 @@
+declare module '@env' {
+  export const API_BASE: string;
+  export const GOOGLE_MAPS_API_KEY: string;
+  export const STRIPE_MERCHANT_IDENTIFIER: string;
+  export const STRIPE_URL_SCHEME: string;
+  export const STRIPE_PUBLISHABLE_KEY: string;
+  export const LUPA_MEETING_SERVICE_FEE_PERCENTAGE: number;
+  export const LUPA_PROGRAM_SERVICE_FEE_PERCENTAGE: number;
+  export const STRIPE_SECRET_KEY: string;
+  export const FIREBASE_API_KEY: string;
+  export const FIREBASE_AUTH_DOMAIN: string;
+  export const FIREBASE_PROJECT_ID: string;
+  export const FIREBASE_STORAGE_BUCKET: string;
+  export const FIREBASE_MESSAGING_SENDER_ID: string;
+  export const FIREBASE_APP_ID: string;
+  export const FIREBASE_MEASUREMENT_ID: string;
+}
+
+declare module '@envDevelopment' {
+  export const API_BASE: string;
+  export const GOOGLE_MAPS_API_KEY: string;
+  export const STRIPE_MERCHANT_IDENTIFIER: string;
+  export const STRIPE_URL_SCHEME: string;
+  export const STRIPE_PUBLISHABLE_KEY: string;
+  export const LUPA_MEETING_SERVICE_FEE_PERCENTAGE: number;
+  export const LUPA_PROGRAM_SERVICE_FEE_PERCENTAGE: number;
+  export const STRIPE_SECRET_KEY: string;
+  export const FIREBASE_API_KEY: string;
+  export const FIREBASE_AUTH_DOMAIN: string;
+  export const FIREBASE_PROJECT_ID: string;
+  export const FIREBASE_STORAGE_BUCKET: string;
+  export const FIREBASE_MESSAGING_SENDER_ID: string;
+  export const FIREBASE_APP_ID: string;
+  export const FIREBASE_MEASUREMENT_ID: string;
+}
