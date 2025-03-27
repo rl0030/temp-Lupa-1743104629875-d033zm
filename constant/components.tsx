ccc07bec5a10f722af31db88a353f1451b7ec02b
rl0030 <r.lewis0030@gmail.com>
2025-03-27 15:48:17 -0400

    Copy constant/components.tsx from Lupa

---
+++ constant/components.tsx
@@ -0,0 +1,8 @@
+import React from 'react-native';
+import {Text, View} from '@gluestack-ui/themed';
+import OutlinedText from '../components/Typography/OutlinedText';
+import {useMemo} from 'react';
+
+const a = 1;
+
+export {a};
