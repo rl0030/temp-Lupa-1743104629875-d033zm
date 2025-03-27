518cf87bbc63fa334da1fd600dab4348b574850c
rl0030 <r.lewis0030@gmail.com>
2025-03-27 15:48:20 -0400

    Copy constant/lupa.ts from Lupa

---
+++ constant/lupa.ts
@@ -0,0 +1,14 @@
+export const FITNESS_CATEGORIES = [
+    'Strength',
+    'Cardiovascular',
+    'Flexibility',
+    'Mobility',
+    'Speed',
+    'Power',
+    'Reaction Time',
+    'Muscular Endurance',
+    'Coordination',
+    'Balance',
+    'Agility',
+    'Bodyweight'
+]
