40dc7b456f61d83b9dc952b6db5d32d0a827cd0a
rl0030 <r.lewis0030@gmail.com>
2025-03-27 15:54:24 -0400

    Copy types/activities/seminars.ts from Lupa

---
+++ types/activities/seminars.ts
@@ -0,0 +1,23 @@
+import {UID} from '../common';
+
+export type PurchaseSeminarVariables = {
+  userId: string;
+  seminarId: string;
+};
+
+export type Seminar = {
+  id: string; // matches to uid
+  uid: string; // matches to id
+  start_time: string;
+  end_time: string;
+  date: string;
+  date_in_utc: string;
+  date_only: string;
+  name: string;
+  user_slots: UID[]; // Array of UIDs
+  pricing: {value: number};
+  max_slots: number;
+  location: {lng: number; lat: number; gym_name: string};
+  trainer_uid: string;
+  metadata: {};
+};
