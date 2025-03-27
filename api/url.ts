d743acaa0fcb92fa6a063ee99e83dc8a69bfef65
rl0030 <r.lewis0030@gmail.com>
2025-03-27 15:45:12 -0400

    Copy api/url.ts from Lupa

---
+++ api/url.ts
@@ -0,0 +1,3 @@
+export const STRIPE_API_CUSTOMERS = 'https://api.stripe.com/v1/customers'
+export const STRIPE_API_PAYMENT_INTENTS = 'https://api.stripe.com/v1/payment_intents'
+export const STRIPE_API_TRANSFERS = 'https://api.stripe.com/v1/transfers'
