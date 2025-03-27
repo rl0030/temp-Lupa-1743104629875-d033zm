ac18066761b70a3ce97aa40abc3adc24fc9556f8
rl0030 <r.lewis0030@gmail.com>
2025-03-27 15:55:01 -0400

    Copy util/time.ts from Lupa

---
+++ util/time.ts
@@ -0,0 +1,8 @@
+export const formatDuration = duration => {
+    const hours = Math.floor(duration / 3600);
+    const minutes = Math.floor((duration % 3600) / 60);
+    const seconds = duration % 60;
+    return `${hours.toString().padStart(2, '0')}:${minutes
+      .toString()
+      .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  };
